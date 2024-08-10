a68474089a080f4b54d2db54208fea42f8fb3506
brunovhk <brunorick11@gmail.com>
2024-08-10 14:51:10 -0300

    style: update Home page styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { StyleSheet, Image } from "react-native";
+import { StyleSheet, Image, Dimensions } from "react-native";
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
 import { TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
+
+const { width } = Dimensions.get("window");
 
 const HomeScreen: React.FC = () => {
   const router = useRouter();
@@ -13,23 +15,62 @@ const HomeScreen: React.FC = () => {
   };
 
   return (
-    <ThemedView>
+    <ThemedView style={styles.container}>
       <Image
         source={require("@/assets/images/home_banner.png")}
+        style={styles.image}
         resizeMode="cover"
       />
-      <ThemedText>Bem-vindo ao Catálogo de Produtos</ThemedText>
-      <ThemedText>
+      <ThemedText style={styles.title}>
+        Bem-vindo ao Catálogo de Produtos
+      </ThemedText>
+      <ThemedText style={styles.description}>
         Descubra nossos produtos incríveis e encontre tudo o que você precisa
         com facilidade.
       </ThemedText>
-      <TouchableOpacity onPress={() => handleNavigate("/products")}>
-        <ThemedText>Ver Catálogo</ThemedText>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => handleNavigate("/products")}
+      >
+        <ThemedText style={styles.buttonText}>Ver Catálogo</ThemedText>
       </TouchableOpacity>
     </ThemedView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  description: {
+    fontSize: 16,
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  image: {
+    width: width,
+    height: 200,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#007bff",
+    padding: 15,
+    borderRadius: 8,
+    marginVertical: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+});
 
 export default HomeScreen;
