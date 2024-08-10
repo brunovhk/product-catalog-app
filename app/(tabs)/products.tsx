0187eec7c45a9a7e6a31d4a638128f34a598afe4
brunovhk <brunorick11@gmail.com>
2024-08-10 17:36:01 -0300

    feat: generate random products with placeholder images using Faker lib

--- app/(tabs)/products.tsx
+++ app/(tabs)/products.tsx
@@ -2,16 +2,25 @@ import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import ProductList from "@/components/ProductList";
 import { ThemedView } from "@/components/ThemedView";
+import { pt_BR, fakerPT_BR } from "@faker-js/faker";
 
 // Dados de exemplo para lista de produtos
 const generateProducts = (count: number) => {
   const products = [];
   for (let i = 1; i <= count; i++) {
+    const productName = fakerPT_BR.commerce.product();
     products.push({
       id: i.toString(),
-      name: `Product ${i}`,
-      price: `R$ ${i * 12}`,
-      image: `https://via.placeholder.com/100?text=Produto+${i}`,
+      name: productName,
+      price: `R$ ${fakerPT_BR.commerce.price()}`,
+      image: fakerPT_BR.image.urlPlaceholder({
+        text: productName,
+        width: 150,
+        height: 150,
+        backgroundColor: "4d79b8",
+        textColor: "000000",
+        format: "png",
+      }),
     });
   }
   return products;
